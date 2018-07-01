dd5ce2b6723483568875a376ad0069c95caa5c0e
Krystal Klumpp <kklumpp13@gmail.com>
2018-07-01 17:03:15 -0600

    Add CSS grid and switch to rem units

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1Ijoia2tsdW1wcDEzIiwiYSI6ImNqajJkb3g2cjBnOXYzcG1xMGozaGxtOWoifQ.NZF2bsbh0BtjB150rE1vhg',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
